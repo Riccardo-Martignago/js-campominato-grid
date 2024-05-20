83a6581df3518b28d90204a89f942ba9c018cf55
Riccardo-Martignago <martigriccardo97@gmail.com>
2024-05-20 18:03:18 +0200

    Aggiunta classe active nel file css, creato un ascoltatore di eventi sul bottone per creare la griglia e un'altra ascoltatore che fa cambiare lo sfondo alle celle al click

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,13 @@
-console.log('Hello!')
+const buttonEl = document.querySelector('button');
+const gridEl = document.querySelector('section.griglia');
+buttonEl.addEventListener('click',function (){
+    for(let i = 0; i < 100; i++){
+        const articleEl = document.createElement('article');
+        articleEl.addEventListener('click', function(){
+            articleEl.classList.add('active')
+            console.log(i+1)
+        })
+        gridEl.appendChild(articleEl);
+        articleEl.append(i+1)
+    }
+})
